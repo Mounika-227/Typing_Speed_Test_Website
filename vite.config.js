18d5d3d8ea65d9e7232c44bff835f71645298e38
T.Mounika <mounikatellabati1@gmail.com>
2026-03-10 23:10:45 +0530

    Fix Vite base path and update app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/Typing_Speed_Test_Website/',
   plugins: [react()],
-})
+  base: "./", // ← add this line
+});
